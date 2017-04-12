e8dba2d5d52e12ac2a3b1384bd1c3ccd67d22a33
Csaba V <csabavari90@gmail.com>
2017-04-13 01:17:58 +0200

    Attempting to integrate PrepareWordVector

--- src/main/java/hu/elte/recipeanalyzer/Main.java
+++ src/main/java/hu/elte/recipeanalyzer/Main.java
@@ -1,8 +1,16 @@
 package hu.elte.recipeanalyzer;
 
+import org.datavec.api.util.ClassPathResource;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("Hello Recipe Analyzer");
+        System.out.println("---");
+        System.out.println("PrepareWordVector test run");
+
+        //System.out.println(new ClassPathResource(".").getFile().getAbsolutePath());
+        PrepareWordVector.main(null);
+        System.out.println("---");
     }
 }
