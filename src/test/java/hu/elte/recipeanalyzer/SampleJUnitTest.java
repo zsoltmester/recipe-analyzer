4cab003134159ba4f0cc886be9b0778f6d982aa7
zsmester <zsolt.mester67@gmail.com>
2017-04-29 15:34:17 +0200

    Remove Main.java.

--- src/test/java/hu/elte/recipeanalyzer/SampleJUnitTest.java
+++ src/test/java/hu/elte/recipeanalyzer/SampleJUnitTest.java
@@ -1,22 +1,20 @@
 package hu.elte.recipeanalyzer;
 
-import junit.framework.TestCase;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
 
 public class SampleJUnitTest {
 
-    private Main main;
+    //private Main main;
 
     @Before
     public void before() {
-        main = Mockito.mock(Main.class);
+        //main = Mockito.mock(Main.class);
     }
 
     @Test
     public void mySampleTest() throws Exception {
-        Mockito.when(main.toString()).thenReturn("ASD");
-        TestCase.assertEquals("ASD", main.toString());
+        //Mockito.when(main.toString()).thenReturn("ASD");
+        //TestCase.assertEquals("ASD", main.toString());
     }
 }
